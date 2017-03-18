34b8d811e1a64d8886dc06f72cb017f61ea356ab
zyisrad <zy@zyisrad.com>
2017-03-17 20:08:43 -0700

    Add comments to Events class

--- h/Events.h
+++ h/Events.h
@@ -2,7 +2,6 @@
 #define __EVENTS__
 
 #include <SDL2/SDL.h>
-
 #include <unordered_map>
 
 class Events
@@ -10,24 +9,33 @@ class Events
 private:
     using EventMap = std::unordered_map<Uint32, void (Events::*)(SDL_Event&)>;
 
+    /**
+     * Map of event codes to member functions
+     */
     EventMap event_map = {
         {
             SDL_KEYDOWN, &Events::keydown
         }
     };
 
+public:
+    /**
+     * Call the event defined in event_map
+     * @param events
+     */
+    void run(SDL_Event& events)
+    {
+        if (event_map.count(events.type))
+            (this->*event_map[events.type])(events);
+    }
+
+private:
     void keydown(SDL_Event& events)
     {
         std::cout << "Key Code: " << events.key.keysym.scancode << "\t" <<
             "Name: " << SDL_GetScancodeName(events.key.keysym.scancode) << "\n";
     }
 
-public:
-    void run(SDL_Event& events)
-    {
-        if (event_map.count(events.type))
-            (this->*event_map[events.type])(events);
-    }
 };
 
 #endif
